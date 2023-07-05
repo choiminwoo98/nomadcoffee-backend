424635fa6e402eadde6fa0a11f3dc41e52f42f95
choiminwoo98 <61531483+choiminwoo98@users.noreply.github.com>
2023-07-05 20:06:59 +0900

    User model 생성&createAccount

--- users/users.mutations.ts
+++ users/users.mutations.ts
@@ -1,12 +1,51 @@
 import client from "../client";
+import * as bcrypt from "bcrypt";
+interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  name: string;
+}
+
 export default {
   Mutation: {
-    createUser: (_: any, { email, name }: { email: string; name?: string }) =>
-      client.user.create({
-        data: {
-          email,
-          name,
-        },
-      }),
+    createAccount: async (
+      _: any,
+      { email, username, name, password }: IUser
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        if (existingUser) {
+          return {
+            ok: false,
+            error: "this username/password is already taken",
+          };
+        }
+        const user = await client.user.create({
+          data: {
+            username,
+            email,
+            name,
+            password: hashedPassword,
+          },
+        });
+        return { ok: true };
+      } catch (e) {
+        return e;
+      }
+    },
   },
 };
--- users/users.typeDefs.ts
+++ users/users.typeDefs.ts
@@ -2,13 +2,23 @@ import { gql } from "apollo-server";
 export default gql`
   type User {
     id: Int!
+    username: String!
     email: String!
-    name: String
+    name: String!
+    location: String
+    password: String!
+    avatarURL: String
+    githubUsername: String
   }
   type Query {
     user(id: Int!): User
   }
   type Mutation {
-    createUser(email: String!, name: String!): User
+    createAccount(
+      email: String!
+      username: String!
+      password: String!
+      name: String!
+    ): User
   }
 `;
